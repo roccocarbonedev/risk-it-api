b02b11d228d29ec9d06b3343e6ba812dd1139054
roccocarbonedev <carbone.r.developer@gmail.com>
2024-06-05 10:50:14 +0200

    third commit

--- src/server.js
+++ src/server.js
@@ -6,22 +6,29 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb+srv://carbonerdeveloper:EEpS0t8hrpU9Y2UQ@cluster0.pfvogjs.mongodb.net/riskitapp').then(function () {
 
+mongoose.connect('mongodb+srv://<username>:<password>@<cluster-url>/<dbname>?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    ssl: true
+}).then(() => {
     app.get('/', function (req, res) {
         res.json({
-            message: 'Api is Working !',
-        })
-    })
+            message: "API is Working !"
+        });
+    });
 
     const roomRouter = require('./routes/Room');
     app.use('/rooms', roomRouter)
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.error('Failed to connect to MongoDB', err);
+});
 
 
-})
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log("Server running at PORT: " + PORT);
 });
